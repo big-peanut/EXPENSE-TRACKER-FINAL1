f90198b85794ec4d6d70913b62cdea2f5aa77b30
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-06-23 20:49:38 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 const Users = require('../models/users')
+const bcrypt = require('bcrypt')
 
 exports.signup = async (req, res, next) => {
     try {
@@ -6,19 +7,22 @@ exports.signup = async (req, res, next) => {
         const email = req.body.email
         const password = req.body.password
 
-        const existingUser = await Users.findOne({ where: { email: email } })
+        const saltrounds = 10
+        bcrypt.hash(password, saltrounds, async (err, hash) => {
+            const existingUser = await Users.findOne({ where: { email: email } })
 
-        if (existingUser) {
-            res.status(400).json({ error: "Email already registered. Please login" })
-        }
-        else {
-            const data = await Users.create({
-                name: name,
-                email: email,
-                password: password
-            })
-            res.json({ dataValues: data })
-        }
+            if (existingUser) {
+                res.status(400).json({ error: "Email already registered. Please login" })
+            }
+            else {
+                const data = await Users.create({
+                    name: name,
+                    email: email,
+                    password: hash
+                })
+                res.json({ dataValues: data })
+            }
+        })
     }
     catch (err) {
         res.json({ err: "Email already registered. Please login" })
@@ -36,15 +40,21 @@ exports.login = async (req, res, next) => {
             res.status(404).json({ error: "Email not registered, Signup please" })
         }
         else {
-            if (password === existingUser.password) {
-                res.json({ message: "User login successful" })
-            } else {
-                res.status(401).json({ error: "Incorrect password" })
-            }
+            bcrypt.compare(password,existingUser.password,(err,result)=>{
+                if(err){
+                    throw new Error('something went wrong')
+                }
+                if(result===true){
+                    res.status(200).json({message: "USER LOGIN SUCCESSFULL"})
+                }
+                else{
+                    res.status(400).json({error: "Incorect password"})
+                }
+            })
         }
     }
-    catch {
-
+    catch(err) {
+        res.status(500).json({message: err})
     }
 
 }
