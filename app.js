2148a8998fbe3b38be9c69cfb2aadae98d8d4901
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-06-24 09:18:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express=require('express')
 const userRoutes=require('./routes/userRoutes')
+const expenseRoutes=require('./routes/expenseRoutes')
 const sequelize=require('./util/database')
 const bodyparser=require('body-parser')
+const cors=require('cors')
 
 const app=express()
 
 app.use(bodyparser.json())
 
+app.use(cors())
+
 app.use(userRoutes)
+
+app.use(expenseRoutes)
 
 sequelize.sync()
     .then((res) => {
