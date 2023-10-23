e02a8a14323863a1cc0e1fdd8a3795d99cb514bf
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:25:43 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -1,60 +1,68 @@
-const Users = require('../models/users')
-const bcrypt = require('bcrypt')
+// Import required modules and packages
+const Users = require('../models/users'); // Import the 'Users' model
+const bcrypt = require('bcrypt'); // Import the bcrypt library for password hashing
 
+// Controller function for user registration (signup)
 exports.signup = async (req, res, next) => {
     try {
-        const name = req.body.name
-        const email = req.body.email
-        const password = req.body.password
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
 
-        const saltrounds = 10
+        const saltrounds = 10;
+        
+        // Hash the password asynchronously with bcrypt
         bcrypt.hash(password, saltrounds, async (err, hash) => {
-            const existingUser = await Users.findOne({ where: { email: email } })
+            // Check if a user with the same email already exists
+            const existingUser = await Users.findOne({ where: { email: email } });
 
             if (existingUser) {
-                res.status(400).json({ error: "Email already registered. Please login" })
+                res.status(400).json({ error: "Email already registered. Please login" });
             }
             else {
+                // Create a new user with the provided information
                 const data = await Users.create({
                     name: name,
                     email: email,
                     password: hash
-                })
-                res.json({ dataValues: data })
+                });
+                res.json({ dataValues: data });
             }
-        })
+        });
     }
     catch (err) {
-        res.json({ err: "Email already registered. Please login" })
+        res.json({ err: "Email already registered. Please login" });
     }
 }
 
+// Controller function for user login
 exports.login = async (req, res, next) => {
     try {
-        const email = req.body.email
-        const password = req.body.password
+        const email = req.body.email;
+        const password = req.body.password;
 
-        const existingUser = await Users.findOne({ where: { email: email } })
+        // Find the user with the provided email
+        const existingUser = await Users.findOne({ where: { email: email } });
 
         if (!existingUser) {
-            res.status(404).json({ error: "Email not registered, Signup please" })
+            res.status(404).json({ error: "Email not registered, Signup please" });
         }
         else {
-            bcrypt.compare(password,existingUser.password,(err,result)=>{
-                if(err){
-                    throw new Error('something went wrong')
+            // Compare the provided password with the stored hashed password
+            bcrypt.compare(password, existingUser.password, (err, result) => {
+                if (err) {
+                    throw new Error('Something went wrong');
                 }
-                if(result===true){
-                    res.status(200).json({message: "USER LOGIN SUCCESSFULL"})
+                if (result === true) {
+                    res.status(200).json({ message: "USER LOGIN SUCCESSFUL" });
                 }
-                else{
-                    res.status(400).json({error: "Incorect password"})
+                else {
+                    res.status(400).json({ error: "Incorrect password" });
                 }
-            })
+            });
         }
     }
-    catch(err) {
-        res.status(500).json({message: err})
+    catch (err) {
+        res.status(500).json({ message: err });
     }
-
 }
