91ce999816dfb684e1b4ddfdc17dd4d926120dae
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:23:37 +0530

    Update expenses.js

--- models/expenses.js
+++ models/expenses.js
@@ -1,26 +1,30 @@
-const Sequelize=require('sequelize')
+// Import the Sequelize library
+const Sequelize = require('sequelize');
 
-const sequelize=require('../util/database')
+// Import the Sequelize instance configured for a database connection
+const sequelize = require('../util/database');
 
-const Expenses=sequelize.define('expenses',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
+// Define a Sequelize model for 'expenses' table
+const Expenses = sequelize.define('expenses', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     },
-    amount:{
-        type:Sequelize.INTEGER,
-        allowNull:false
+    amount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
     },
-    description:{
-        type:Sequelize.STRING,
-        allowNull:false,
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     },
-    category:{
-        type:Sequelize.STRING,
-        allowNull:false
+    category: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-})
+});
 
-module.exports=Expenses
+// Export the 'Expenses' model to make it available for use in other parts of your application
+module.exports = Expenses;
