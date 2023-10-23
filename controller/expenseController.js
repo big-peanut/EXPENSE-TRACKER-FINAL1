e65ddd0c0b37aa2d28e70714d230d81113f0733a
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:26:30 +0530

    Update expenseController.js

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,44 +1,55 @@
-const Expenses=require('../models/expenses')
+// Import the 'Expenses' model
+const Expenses = require('../models/expenses');
 
-exports.addexpense=async(req,res,next)=>{
-    try{
-        const amount = req.body.amount
-        const description = req.body.description
-        const category = req.body.category
+// Controller function for adding an expense
+exports.addexpense = async (req, res, next) => {
+    try {
+        const amount = req.body.amount;
+        const description = req.body.description;
+        const category = req.body.category;
 
-        const data=await Expenses.create({
-            amount:amount,
-            description:description,
-            category:category
-        })
-        res.json({dataValues:data})
+        // Create a new expense record in the database
+        const data = await Expenses.create({
+            amount: amount,
+            description: description,
+            category: category
+        });
+        res.json({ dataValues: data });
     }
-    catch(err){
-        console.log(err)
-    }
-}
-
-exports.getexpense=async(req,res,next)=>{
-    try{
-        const expenses=await Expenses.findAll()
-        res.json({allexpenses:expenses})
-    }
-    catch(err){
-        console.log(err)
+    catch (err) {
+        console.log(err);
     }
 }
 
-exports.delexpense=async(req,res,next)=>{
-    try{
-        if(req.params.id=='undefined'){
-            console.log("id is missing")
+// Controller function for retrieving all expenses
+exports.getexpense = async (req, res, next) => {
+    try {
+        // Retrieve all expenses from the database
+        const expenses = await Expenses.findAll();
+        res.json({ allexpenses: expenses });
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+// Controller function for deleting an expense by ID
+exports.delexpense = async (req, res, next) => {
+    try {
+        // Check if the ID parameter is missing
+        if (req.params.id == 'undefined') {
+            console.log("ID is missing");
         }
-        const id=req.params.id
-        await Expenses.destroy({where:{id:id}})
-        res.sendStatus(200)
         
+        const id = req.params.id;
+
+        // Delete the expense with the provided ID from the database
+        await Expenses.destroy({ where: { id: id } });
+        
+        // Send a 200 OK response to indicate successful deletion
+        res.sendStatus(200);
     }
-    catch(err){
-        console.log(err)
+    catch (err) {
+        console.log(err);
     }
 }
