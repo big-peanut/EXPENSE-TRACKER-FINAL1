95e96628d05758588ec31e71d9452037fc2fc9da
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:22:42 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,27 +1,31 @@
-const Sequelize=require('sequelize')
+// Import the Sequelize library
+const Sequelize = require('sequelize');
 
-const sequelize=require('../util/database')
+// Import the Sequelize instance configured for database conection
+const sequelize = require('../util/database');
 
-const Users=sequelize.define('users',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
+// Define a Sequelize model for 'users' table
+const Users = sequelize.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     },
-    name:{
-        type:Sequelize.STRING,
-        allowNull:false
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
     },
-    email:{
-        type:Sequelize.STRING,
-        allowNull:false,
-        unique:true
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
     },
-    password:{
-        type:Sequelize.STRING,
-        allowNull:false
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-})
+});
 
-module.exports=Users
+// Export the 'Users' model to make it available for use in other parts of your application
+module.exports = Users;
