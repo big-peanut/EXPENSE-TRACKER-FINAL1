b865655664b72b7cffefb899eaa7831bc46053e7
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:16:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const express=require('express')
-const userRoutes=require('./routes/userRoutes')
-const expenseRoutes=require('./routes/expenseRoutes')
-const sequelize=require('./util/database')
-const bodyparser=require('body-parser')
-const cors=require('cors')
+// Import required modules and packages
+const express = require('express'); // Import the Express.js framework
+const userRoutes = require('./routes/userRoutes'); // Import user routes
+const expenseRoutes = require('./routes/expenseRoutes'); // Import expense routes
+const sequelize = require('./util/database'); // Import the Sequelize database instance
+const bodyparser = require('body-parser'); // Import the body-parser middleware for parsing JSON request bodies
+const cors = require('cors'); // Import the CORS middleware for handling Cross-Origin Resource Sharing
 
-const app=express()
+// Create an instance of the Express application
+const app = express();
 
-app.use(bodyparser.json())
+// Parse JSON request bodies
+app.use(bodyparser.json());
 
-app.use(cors())
+// Enable CORS to allow cross-origin requests
+app.use(cors());
 
-app.use(userRoutes)
+// Define routes for handling user-related requests
+app.use(userRoutes);
 
-app.use(expenseRoutes)
+// Define routes for handling expense-related requests
+app.use(expenseRoutes);
 
+// Synchronize the Sequelize database with the defined models
 sequelize.sync()
     .then((res) => {
-        app.listen(3000)
+        // Start the Express server on port 3000 once the database is synchronized
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
     })
     .catch((err) => {
-        console.log(err)
-    })
+        console.log(err);
+    });
