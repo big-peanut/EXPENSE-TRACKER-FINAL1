1940cb062f5e1a93c619c36762ce6a2773cb15aa
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:18:30 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,5 +1,11 @@
-const Sequelize=require('sequelize')
+// Import the Sequelize library
+const Sequelize = require('sequelize');
 
-const sequelize=new Sequelize('expensetracker','root','12345678',{dialect:'mysql',host:'localhost'})
+// Create a new Sequelize instance and configure it to connect to a MySQL database
+const sequelize = new Sequelize('expensetracker', 'root', '12345678', {
+    dialect: 'mysql', // Specify the database type (in this case, MySQL)
+    host: 'localhost' // Specify the database server's host (in this case, localhost)
+});
 
-module.exports=sequelize
+// Export the configured Sequelize instance to make it available for use in other parts of your application
+module.exports = sequelize;
