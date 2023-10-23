b54f5499cf483c5d585fdd155b7dca3571da84cb
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:20:40 +0530

    Update expenseRoutes.js

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,10 +1,14 @@
-const express=require('express')
-const controller=require('../controller/expenseController')
+// Import required modules and packages
+const express = require('express'); // Import the Express.js framework
+const controller = require('../controller/expenseController'); // Import the expense controller
 
-const router=express.Router()
+// Create a new Express Router instance
+const router = express.Router();
 
-router.post('/expense/addexpense',controller.addexpense)
-router.get('/expense/getexpense',controller.getexpense)
-router.delete('/expense/delexpense/:id',controller.delexpense)
+// Define routes for expense-related actions
+router.post('/expense/addexpense', controller.addexpense); // Route for adding an expense
+router.get('/expense/getexpense', controller.getexpense); // Route for retrieving expenses
+router.delete('/expense/delexpense/:id', controller.delexpense); // Route for deleting an expense by its ID
 
-module.exports=router
+// Export the router to make it available for use in other parts of your application
+module.exports = router;
