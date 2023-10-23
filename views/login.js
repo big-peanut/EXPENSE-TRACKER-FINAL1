da9aa03e3f344ed085f9188d4887a13eb71e6765
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-10-23 08:28:07 +0530

    Update login.js

--- views/login.js
+++ views/login.js
@@ -1,5 +1,7 @@
-const loginform = document.getElementById('loginform')
+// Get a reference to the login form element in the HTML
+const loginform = document.getElementById('loginform');
 
+// Function for user login
 async function login(email, password) {
     try {
         const user = {
@@ -7,30 +9,40 @@ async function login(email, password) {
             password
         };
 
+        // Send a POST request to the server for user login
         const response = await axios.post("http://localhost:3000/login", user);
 
+        // Check the response from the server
         if (response.data.message) {
+            // If the response contains a 'message', show a success alert
             alert("User login successful");
         } else {
+            // If the response does not contain a 'message', show an alert for login failure
             alert("Login failed. Incorrect password or email not registered.");
         }
     } catch (err) {
         if (err.response && err.response.data) {
+            // If there is a response from the server with an error message, show that error
             alert(err.response.data.error);
         } else {
-            alert("An error occurred during login:", err.message);
+            // If there is a general error without a specific response, show a generic error message
+            alert("An error occurred during login: " + err.message);
         }
     }
 }
 
+// Event listener for form submission
 loginform.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    let email = document.getElementById('email').value
-    let password = document.getElementById('password').value
+    // Get the user input values from the form
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
 
-    login(email, password)
+    // Call the 'login' function with the provided email and password
+    login(email, password);
 
-    email = ""
-    password = ""
-})
+    // Clear the input fields
+    email = "";
+    password = "";
+});
